db92c865e48d11042d672ab00eda6b5e8d1eae96
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-04-20 15:17:11 +0200

    added an environment variable for better configuration

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(
       cookie: {
          maxAge: 1000 * 60 * 60 * 24,
          sameSite: "none",
-         secure: false,
+         secure: process.env.HTTPS,
          httpOnly: true
       },
       store: new MemoryStore({
