fabae3c36056c3a10a8857d964b428d6b2d4b902
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-04-21 16:25:10 +0200

    update of endpoint in categories

--- server/categories.js
+++ server/categories.js
@@ -15,8 +15,9 @@ categoriesRouter.get("/categories", async (request, response) => {
 
 categoriesRouter.get("/categories/:id", async (request, response) => {
    try {
+      console.log(request.params.id)
       const result = await db.query(
-         "SELECT items.id, items.name, description, categories.name, price, discount, price*((100-discount) / 100) as newprice FROM items JOIN prices ON prices.items_id = items.id JOIN items_categories ON prices.items_id = items_categories.items_id JOIN categories ON items_categories.categories_id = categories.id WHERE categories.id = $1",
+         "SELECT items.id, items.name, description, image_path, categories.name as category, price, discount, price*((100-discount) / 100) as newprice FROM items JOIN prices ON prices.items_id = items.id JOIN items_categories ON prices.items_id = items_categories.items_id JOIN categories ON items_categories.categories_id = categories.id WHERE categories.id = $1",
          [request.params.id]
       )
       response.send(result.rows)
